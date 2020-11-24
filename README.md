67878b352d40d99083cba8486b5518c4274935db
Max <32428314+benkenobi3@users.noreply.github.com>
2020-11-25 01:11:16 +0300

    add docs

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
-# test
-Тестовое задание для стажера в Market Intelligence
+## Test task in the Market Intelligence unit
+
+### Quick start:
+    $ docker-compose up
+
+### Api methods:
+
+#### /add
+The method receives 'phrase' and 'region' fields as input, creates Counter and returns 'counter_id'
+
+##### Request body example:
+    {
+      'phrase': 'string',
+      'region': 'string'
+    }
+
+##### Successful response example:
+    {
+      'counter_id': 'string'
+    }
+
+##### Validation Error:
+    {
+      "detail": [
+        {
+          "loc": [
+            "string"
+          ],
+          "msg": "string",
+          "type": "string"
+        }
+      ]
+    }
+
+##### Not found error:
+    {
+      "msg": "Region not found"
+    }
+
+
+#### /stat
+The method receives 'counter_id' and two timestamp fields: 'time_from', 'time_to'; and returns list of counter's results
+
+##### Request body example:
+    {
+      'counter_id': 'string',
+      'time_from': 'int',
+      'time_to': 'int'
+    }
+
+##### Successful response example:
+    [
+      {
+        'count': 'int',
+        'timestamp': 'int'
+      },
+      ...
+    ]
+
+##### Validation Error:
+    {
+      "detail": [
+        {
+          "loc": [
+            "string"
+          ],
+          "msg": "string",
+          "type": "string"
+        }
+      ]
+    }
+
+##### Not found error:
+    {
+      "msg": "No results with provided arguments"
+    }
+
+#### /stat/with_top
+Does the same as '/stat' but adds additional field to response
+
+##### Successful response example:
+    [
+      {
+        'count': 'int',
+        'timestamp': 'int',
+        'top_ads': 'string'
+      },
+      ...
+    ]
--- src/app/fastapi.py
+++ src/app/fastapi.py
@@ -14,6 +14,19 @@ app = FastAPI()
 
 @app.post('/add', status_code=201)
 async def add(phrase: str = Form(...), region: str = Form(...)) -> Response:
+    """The method receives 'phrase' and 'region' as input, creates Counter and returns 'counter_id'
+
+    Request body example
+    {
+      'phrase': 'string',
+      'region': 'string'
+    }
+
+    Successful response example:
+    {
+      'counter_id': 'string'
+    }
+    """
     region_id: int = get_region_id(region)
     if region_id:
         counter: Counter = await add_counter(
@@ -21,24 +34,55 @@ async def add(phrase: str = Form(...), region: str = Form(...)) -> Response:
         )
         counter.id = str(counter.id)
         count_ads.send_with_options(args=[counter.dict()], delay=DELAY)
-        res = json.dumps({'id': counter.id})
+        res = json.dumps({'counter_id': counter.id})
         return Response(status_code=201, content=res, media_type='application/json')
-    return Response(status_code=400, content='Region not found')
+    return Response(status_code=400, content='{ "msg": "Region not found" }')
 
 
 @app.post('/stat')
 async def stat(counter_id: str = Form(...), time_from: int = Form(...), time_to: int = Form(...)) -> Response:
+    """The method receives 'counter_id' and two timestamp fields: 'time_from', 'time_to';
+    and returns list of counter's results
+
+    Request body example:
+    {
+      'counter_id': 'string',
+      'time_from': 'int',
+      'time_to': 'int'
+    }
+
+    Successful response example:
+    [
+      {
+        'count': 'int',
+        'timestamp': 'int'
+      },
+      ...
+    ]
+    """
     results = await get_results(counter_id=counter_id, time_from=time_from, time_to=time_to)
     if results:
         res = json.dumps(results)
         return Response(status_code=200, content=res, media_type='application/json')
-    return Response(status_code=404, content='No one result with provided arguments')
+    return Response(status_code=404, content='{ "msg: "No results with provided arguments" }')
 
 
 @app.post('/stat/with_top')
 async def top(counter_id: str = Form(...), time_from: int = Form(...), time_to: int = Form(...)) -> Response:
+    """Does the same as '/stat' but adds additional field to response
+
+    Successful response example:
+    [
+      {
+        'count': 'int',
+        'timestamp': 'int',
+        'top_ads': 'string'
+      },
+      ...
+    ]
+    """
     results = await get_results(counter_id=counter_id, time_from=time_from, time_to=time_to, with_top=True)
     if results:
         res = json.dumps(results)
         return Response(status_code=200, content=res, media_type='application/json')
-    return Response(status_code=404, content='No one result with provided arguments')
+    return Response(status_code=404, content='{ "msg: "No results with provided arguments" }')
